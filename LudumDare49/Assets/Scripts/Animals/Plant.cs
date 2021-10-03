fc7a49267f6663669aa8d44a79d41bf1660b231a
Dan <mrdanjh@gmail.com>
2021-10-03 12:43:25 +0100

    Vary plant tint slightly

--- LudumDare49/Assets/Scripts/Animals/Plant.cs
+++ LudumDare49/Assets/Scripts/Animals/Plant.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Plant : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class Plant : MonoBehaviour
     void OnEnable()
     {
         transform.GetChild(0).localScale = new Vector3(Random.value > 0.5f ? 1 : -1, 1, 1);
+
+        Image[] images = GetComponentsInChildren<Image>();
+        Color tint = new Color(Random.Range(0.6f, 1f), Random.Range(0.6f, 1f), Random.Range(0.6f, 1f));
+        foreach(Image image in images)
+            image.color = tint;
     }
 
     void Update()
